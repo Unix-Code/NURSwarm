846d016574a6595d0c20abb5c6263b1289e15ed2
Vishal Ramesh <vishalramesh50@gmail.com>
2020-02-24 21:43:21 -0500

    Clean up some math and add constants

--- drawsim.py
+++ drawsim.py
@@ -1,71 +1,70 @@
-# import pygame module in this program 
 import pygame
-from pygame.locals import *
 import pygame.gfxdraw
+from pygame.locals import Rect
 
-# activate the pygame library . 
-# initiate pygame and give permission 
-# to use pygame's functionality. 
-pygame.init() 
-  
-# define the RGB value 
-# for white, green, 
-# blue, black, red 
-# colour respectively. 
-white = (255, 255, 255) 
-green = (0, 255, 0) 
-light_purple = (50, 50, 128)
-black = (0, 0, 0) 
-grey = (200, 200, 200)
-# assigning values to X and Y variable 
-X = 400
-Y = 400
-  
-# create the display surface object 
-# of specific dimension..e(X,Y). 
-display_surface = pygame.display.set_mode((X, Y)) 
-  
-# # set the pygame window name 
-# pygame.display.set_caption('Drawing') 
-  
-# completely fill the surface object  
-# with white colour  
-display_surface.fill(grey) 
+# activate the pygame library .
+# initiate pygame and give permission
+# to use pygame's functionality.
+pygame.init()
 
-# axle size
-axle_width = 150
-axle_height = 20
+# RGB color constants
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+LIGHT_PURPLE = (50, 50, 128)
+BLACK = (0, 0, 0)
+GREY = (200, 200, 200)
 
-# wheel size
-wheel_size = 30
+# background width and height
+WIDTH = 400
+HEIGHT = 400
 
-# bot size
-bot_size = 150
+# create the display surface object of specific dimension. (WIDTH,HEIGHT).
+display_surface: pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.gfxdraw.aacircle(display_surface, int(X/2), int(Y/2), int(bot_size/2 - 1), green)
-pygame.gfxdraw.filled_circle(display_surface, int(X/2), int(Y/2), int(bot_size/2 - 1), green)
-pygame.gfxdraw.box(display_surface, Rect(X/2 - axle_width/2, Y/2 - axle_height/2, axle_width, axle_height), light_purple)
-pygame.gfxdraw.box(display_surface, Rect(X/2 - axle_width/2 - wheel_size/2, Y/2 - axle_height/2 - (wheel_size - axle_height) / 2, wheel_size, wheel_size), black)
-pygame.gfxdraw.box(display_surface, Rect(X/2 + axle_width/2 - wheel_size/2, Y/2 - axle_height/2 - (wheel_size - axle_height) / 2, wheel_size, wheel_size), black)
-# pygame.gfxdraw.
-  
-# infinite loop 
-while True : 
-      
-    # iterate over the list of Event objects 
-    # that was returned by pygame.event.get() method. 
-    for event in pygame.event.get() : 
-  
-        # if event object type is QUIT 
-        # then quitting the pygame 
-        # and program both. 
-        if event.type == pygame.QUIT : 
-  
-            # deactivates the pygame library 
-            pygame.quit() 
-  
-            # quit the program. 
-            quit() 
-  
-        # Draws the surface object to the screen.  
-        pygame.display.update()  
+# set the pygame window name
+pygame.display.set_caption("Basic Simulation")
+
+# set the background color
+display_surface.fill(GREY)
+
+# bot radius
+BOT_RADIUS = 150
+
+# body of bot
+pygame.gfxdraw.aacircle(display_surface, WIDTH // 2, HEIGHT // 2, BOT_RADIUS // 2, GREEN)
+pygame.gfxdraw.filled_circle(display_surface, WIDTH // 2, HEIGHT // 2, BOT_RADIUS // 2, GREEN)
+
+# axle
+AXLE_WIDTH = 150
+AXLE_HEIGHT = 20
+AXLE_X = WIDTH // 2 - AXLE_WIDTH // 2
+AXLE_Y = HEIGHT // 2 - AXLE_HEIGHT // 2
+pygame.gfxdraw.box(display_surface, Rect(AXLE_X, AXLE_Y, AXLE_WIDTH, AXLE_HEIGHT), LIGHT_PURPLE)
+
+# wheels
+WHEEL_RADIUS = 30
+WHEEL_Y = AXLE_Y - (WHEEL_RADIUS - AXLE_HEIGHT) // 2
+# left wheel
+pygame.gfxdraw.box(display_surface, Rect(AXLE_X - WHEEL_RADIUS // 2, WHEEL_Y, WHEEL_RADIUS, WHEEL_RADIUS), BLACK)
+# right wheel
+pygame.gfxdraw.box(display_surface, Rect(AXLE_X * 2 + WHEEL_RADIUS // 2, WHEEL_Y, WHEEL_RADIUS, WHEEL_RADIUS), BLACK)
+
+# infinite loop
+while True:
+
+    # iterate over the list of Event objects
+    # that was returned by pygame.event.get() method.
+    for event in pygame.event.get():
+
+        # if event object type is QUIT
+        # then quit the pygame and program.
+        if event.type == pygame.QUIT:
+
+            # deactivates the pygame library
+            pygame.quit()
+
+            # quit the program.
+            quit()
+
+        # Draws the surface object to the screen.
+        pygame.display.update()
