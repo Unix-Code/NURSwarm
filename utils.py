3769aa640716a7f1a39a0a09536aaf671cf907ab
Unix-Code <dmalakh@comcast.net>
2020-03-02 21:53:15 -0500

    update Vector class

--- utils.py
+++ utils.py
@@ -1,67 +1,90 @@
-from math import cos, sin, sqrt
+from math import cos, sin, sqrt, atan2, degrees
 from functools import reduce
 
 
 class Vector:
-    def __init__(self, x, y, speed):
+    MAX_SPEED = 10
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.speed = speed
-        self.MAX_SPEED = 3  # Arbitrary max speed for steering
+        self.mag = euclid_distance((0, 0), (x, y))
 
     def normalize(self):
-        return Vector(0, 0, 0) if self.speed == 0 else self.div(self.speed);
+        return Vector(0, 0) if self.mag == 0 else self.div(self.mag)
 
     def add(self, other_vector):
         new_x = self.x + other_vector.x
         new_y = self.y + other_vector.y
-        new_speed = sqrt(new_x ** 2 + new_y ** 2)
-        return Vector(new_x, new_y, new_speed)
-
-    def add_many(self, other_vectors):
-        return self.add(reduce(lambda a, b: a.add(b), other_vectors))
+        return Vector(new_x, new_y)
 
     def sub(self, other_vector):
         new_x = self.x - other_vector.x
         new_y = self.y - other_vector.y
-        new_speed = euclid_distance((0, 0), (new_x, new_y))
-        return Vector(new_x, new_y, new_speed)
+        return Vector(new_x, new_y)
 
     def mult(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar, self.speed * scalar)
+        return Vector(self.x * scalar, self.y * scalar)
+
+    def mult_complex(self, scalarx, scalary):
+        return Vector(self.x * scalarx, self.y * scalary)
 
     def div(self, scalar):
-        return Vector(self.x / scalar, self.y / scalar, self.speed / scalar)
+        return Vector(self.x / scalar, self.y / scalar)
 
     def invert(self):
-        return Vector(self.x * -1, self.y * -1, self.speed)
+        return Vector(self.x * -1, self.y * -1)
+
+    def invert_x(self):
+        return Vector(self.x * -1, self.y)
+
+    def invert_y(self):
+        return Vector(self.x, self.y * -1)
 
     def set_mag(self, new_mag):
         return self.normalize().mult(new_mag)
 
-    def limit(self, max):
-        length_squared = self.x ** 2 + self.y ** 2
-        ratio = 1
-        if length_squared > max ** 2 and length_squared > 0:
-            ratio = max / sqrt(length_squared)
+    def limit(self, limit=MAX_SPEED):
+        return self.normalize().mult(limit) if self.mag > limit else self
 
-        new_x = self.x * ratio
-        new_y = self.y * ratio
-        new_speed = euclid_distance((0, 0), (new_x, new_y))
-        return Vector(new_x, new_y, new_speed)
+    def heading(self, in_degrees=False):
+        angle = atan2(self.y, self.x)
+        return degrees(angle) if in_degrees else angle
 
     def coords(self):
-        return (self.x, self.y)
+        return self.x, self.y
 
-def average_from_vectors(vectors):
-    x_coords = [vector.x for vector in vectors]
-    y_coords = [vector.y for vector in vectors]
-    speeds = [vector.speed for vector in vectors]
-    avg_x = average(x_coords)
-    avg_y = average(y_coords)
-    avg_speed = average(speeds)
-    avg_vector = Vector(avg_x, avg_y, avg_speed)
-    return avg_vector
+    def polar_coords(self):
+        """
+        Cartesion -> Polar
+        :return: polar coordinates in format (r, theta)
+        """
+        return self.mag, self.heading()
+
+    @classmethod
+    def steer_force(cls, target, location, velocity, max_force, max_speed=MAX_SPEED):
+        return target.sub(location).set_mag(max_speed).sub(velocity).limit(max_force)
+
+    @classmethod
+    def steer_forces(cls, targets, location, velocity, max_force, max_speed=MAX_SPEED):
+        target_vels = [target.sub(location) for target in targets]
+        weighted_target_vels = [t.mult(euclid_distance(Vector.null().coords(), t.coords()) ** 2) for t in target_vels]
+        return Vector.average_many(weighted_target_vels).set_mag(max_speed).sub(velocity).limit(max_force)
+
+    @classmethod
+    def null(cls):
+        return Vector(0, 0)
+
+    @classmethod
+    def average_many(cls, vectors):
+        sum_x = 0
+        sum_y = 0
+        for vector in vectors:
+            sum_x += vector.x
+            sum_y += vector.y
+        avg_x = sum_x / len(vectors)
+        avg_y = sum_y / len(vectors)
+        return Vector(avg_x, avg_y)
 
 
 def euclid_distance(p0, p1):
@@ -70,6 +93,7 @@ def euclid_distance(p0, p1):
     p1 : (x, y)
     """
     return sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
 
 def convert_polar_to_cartesian(r, theta):
     """Returns cartesian coordinates from polar coordinates
